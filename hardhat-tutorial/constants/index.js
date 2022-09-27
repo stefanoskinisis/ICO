0c16bca387030fe5083385f49d0d300a0da9d020
stefanoskinisis <stefanoskinisis@gmail.com>
2022-09-27 15:49:08 +0300

    new file:   hardhat-tutorial/.gitignore 	new file:   hardhat-tutorial/README.md 	new file:   hardhat-tutorial/constants/index.js 	new file:   hardhat-tutorial/contracts/CryptoDevToken.sol 	new file:   hardhat-tutorial/contracts/ICryptoDevs.sol 	new file:   hardhat-tutorial/hardhat.config.js 	new file:   hardhat-tutorial/package-lock.json 	new file:   hardhat-tutorial/package.json 	new file:   hardhat-tutorial/scripts/deploy.js 	new file:   hardhat-tutorial/test/Lock.js 	new file:   my-app

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x9ef612bB1DaB08b556758528e9885c9180aA6836";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
